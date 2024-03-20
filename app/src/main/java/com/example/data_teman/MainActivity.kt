ed0a64d2179f17c61ceddf6c945841bf7486d20f
H3ndric0 <hendricosuryasaputra16@gmail.com>
2024-03-20 11:38:45 +0700

    third

--- app/src/main/java/com/example/data_teman/MainActivity.kt
+++ app/src/main/java/com/example/data_teman/MainActivity.kt
@@ -13,6 +13,8 @@ import com.firebase.ui.auth.AuthUI
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private var auth:FirebaseAuth? = null
@@ -37,7 +39,44 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(p0: View?) {
         when (p0?.getId()) {
-            R.id.save -> {}
+            R.id.save -> {
+                //Mendapatkan userID dari pengguna yang terautententikasi
+                val getUserID = auth!!.currentUser!!.uid
+
+                //mendapatkan Instance dari data base
+                val database = FirebaseDatabase.getInstance()
+
+                //Menyimpan data yang diimputkan user dalam variabel
+                val getNama: String = binding.nama.getText().toString()
+                val getAlamat: String = binding.alamat.getText().toString()
+                val getNoHP: String = binding.noHp.getText().toString()
+
+                //Mendapatkan Refrensi dari database
+                val getReference: DatabaseReference
+                getReference = database.reference
+
+                //mengecek apakah ada data yg kosong
+                if (isEmpty(getNama) || isEmpty(getAlamat) || isEmpty(getNoHP)) {
+                    Toast.makeText(this@MainActivity, "Data tidak boleh ada yang kosong",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    //Jika teidak ada maka menyimpan ke database sesuai ID masing masing akun
+                    getReference.child("Admin").child(getUserID).child("DataTeman").push()
+                        .setValue(data_teman(getNama, getAlamat, getNoHP))
+                        .addOnCompleteListener(this){
+                            //bagian yg terjadi ketika user berhasil menyimpan data
+                            binding.nama.setText("")
+                            binding.alamat.setText("")
+                            binding.noHp.setText("")
+                            Toast.makeText(this@MainActivity, "Data Tersimpan",
+                                Toast.LENGTH_SHORT).show()
+                        }
+
+
+                }
+
+
+            }
             R.id.logout -> {
                 AuthUI.getInstance().signOut(this)
                     .addOnCompleteListener(object : OnCompleteListener<Void> {
---
+++ app/src/main/java/com/example/data_teman/data_teman.kt
@@ -0,0 +1,19 @@
+package com.example.data_teman
+
+class data_teman {
+    //deklarasi var
+    var nama: String? = null
+    var alamat: String? = null
+    var no_hp: String? = null
+    var key: String? = null
+
+    //Membuat konstruktor kosong untuk membaca data snapshot
+    constructor() {}
+
+    //Konstruksi dengan beberapa parameter, untuk mendapatkan input dari user
+    constructor(nama: String?, alamat: String?, no_hp: String?){
+        this.nama  = nama
+        this.alamat = alamat
+        this.no_hp = no_hp
+    }
+}
